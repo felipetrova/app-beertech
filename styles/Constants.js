2ebe65300f0a4fddce391a634043864359d3d498
Felipe Trova <felipee.trova@gmail.com>
2020-04-22 19:50:01 -0300

    Add basic and global styles + add public assets

---
+++ styles/Constants.js
@@ -0,0 +1,15 @@
+// Grid
+export const CONTAINER_WIDTH = 1200;
+export const NUMBER_COLUMN = 12;
+export const GUTTER_WIDTH = 30;
+
+// Colors
+export const BLUE = '#0072c6';
+export const GREEN = '#00c458';
+export const RED = '#ff7473';
+export const ORANGE = '#F36D16';
+export const LIGHT_GREY = '#CCCCCC';
+export const MEDIUM_GREY = '#181818';
+export const DARK_GREY = '#121212';
+export const BLACK = '#000000cc';
+export const WHITE = '#FFFFFF';
